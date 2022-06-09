1c453414b24a6e4bc39cfaa35586d9ce2904d00b
Hugo Estevam Silva Meirelles <hugoestevam@gmail.com>
2022-06-09 13:49:42 -0300

    atualização para mudar de maquina

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,15 @@ import { UsuariosModule } from './../usuarios/usuarios.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { jwtConstantes } from './constants';
 
 @Module({
-  imports:[UsuariosModule, PassportModule],
+  imports:[UsuariosModule, 
+           PassportModule,
+          JwtModule.register({
+            secret: jwtConstantes.secret
+          })],
   providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
